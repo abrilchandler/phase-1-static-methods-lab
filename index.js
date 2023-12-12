a4354916c4919ea0905779f73bbb234fd1d46223
Chandler Abril <abrilchandler1@gmail.com>
2023-12-11 22:10:05 -0700

    Done

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "")
+  }
+
+  static titleize(string) {
+    const excludeWords = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const newArr = [];
+    const wordArray = string.split(' ');
+    for (let i = 0; i < wordArray.length; i++) {
+      if (!excludeWords.includes(wordArray[i])) {
+        newArr.push(this.capitalize(wordArray[i]))
+      } else {
+        newArr.push(wordArray[i])
+      }
+    }
+  return Formatter.capitalize(newArr.join(' '))
+}
 }
